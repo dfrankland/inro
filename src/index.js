c0594aaa1560dc329b360dd38e3db9d249edb18a
Dylan Frankland <dylan@frankland.io>
2017-08-24 15:43:24 -0700

    Add constants to main file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as components from './components';
 import * as hocs from './hocs';
 import * as lib from './lib';
+import * as constants from './constants';
 
 export default {
   ...components,
   ...hocs,
   ...lib,
+  constants,
 };
